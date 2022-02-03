ee70dd5eac0a292c132cdf4d060f78f360f29afa
blade <yiming.liao@cuw.edu>
2022-02-03 11:34:38 -0600

    add util function which can generate csv from graph table and read csv

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import util
+
+
 class Graph:
     def __init__(self, vertices):
         self.V = vertices
@@ -11,6 +14,7 @@ class Graph:
         return minVal
 
     def MST_Process(self):
+        minLength = 0
         MST = [False] * self.size
         L = []
         E = []
@@ -27,13 +31,21 @@ class Graph:
                 MST[edge[0]] = True
                 MST[edge[1]] = True
                 E = []
+                minLength = minLength + edge[2]
         print(L)
+        print('\npath length is: ', minLength)
 
 
-g = Graph([
-    [0, 2,  6,  12],
-    [2, 0,  10, 5],
-    [6, 10, 0,  4],
-    [12,5,  4,  0]
-])
-g.MST_Process()
+#g = Graph([
+#    [0, 2,  6,  12],
+#    [2, 0,  10, 5],
+#    [6, 10, 0,  4],
+#    [12,5,  4,  0]
+#])
+#g.MST_Process()
+u = util.util()
+#v = u.generateGraph(400, 20)
+#print(v)
+#u.generateGraphCSV(400, 20)
+#g = Graph(u.readCSV())
+#g.MST_Process()
---
+++ util.py
@@ -0,0 +1,40 @@
+import random
+import numpy as np
+import csv
+import pandas as pd
+
+
+class util:
+    def __init__(self):
+        pass
+
+    def generateGraph(self, size, max):
+        v = []
+        for i in range(size):
+            r = [0] * size
+            for j in range(size):
+                if j == i:
+                    r[j] = 0
+                elif j < i:
+                    r[j] = v[j][i]
+                else:
+                    r[j] = random.randint(1, max)
+            v.append(r)
+        return v
+
+    def generateGraphCSV(self, size, max):
+        v = self.generateGraph(size, max)
+        with open('graph.csv', 'w', newline='') as file:
+            mywriter = csv.writer(file, delimiter=',')
+            mywriter.writerows(v)
+
+    def readCSV(self):
+        v = []
+        with open('graph.csv', 'r', newline='') as file:
+            myreader = csv.reader(file, delimiter=',')
+            for rows in myreader:
+                r = []
+                for i in rows:
+                    r.append(int(i))
+                v.append(r)
+        return v
